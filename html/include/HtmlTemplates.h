7f80b2343d3b74bbfecd95b1e0b5982a6ed294d2
igorJarek <igij@o2.pl>
2020-03-19 10:20:29 +0100

    move html templates to new file header

---
+++ html/include/HtmlTemplates.h
@@ -0,0 +1,167 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+const string INDEX_TEMPLATE
+{
+	"<!DOCTYPE html>\n"
+	"<html lang=\"en\">\n"
+	"<head>\n"
+	"	<meta charset=\"utf-8\"/>\n"
+	"	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\n"
+
+	"	<title><?title?></title>\n\n"
+
+	"	<style>\n"
+	"		@font-face {\n"
+	"			font-family: 'JetBrainsMono-Regular';\n"
+	"			src: url('https://raw.githubusercontent.com/igorJarek/SourceCodeAnalyzer/master/html/fonts/JetBrainsMono-Regular.woff') format('woff');\n"
+	"			font-weight: normal;\n"
+	"			font-style: normal;\n"
+	"		}\n\n"
+
+	"		@font-face {\n"
+	"			font-family: 'JetBrainsMono-Bold';\n"
+	"			src: url('https://raw.githubusercontent.com/igorJarek/SourceCodeAnalyzer/master/html/fonts/JetBrainsMono-Bold.woff') format('woff');\n"
+	"			font-weight: bold;\n"
+	"			font-style: normal;\n"
+	"		}\n\n"
+
+	"		body {\n"
+	"			background-color: rgb(255, 255, 220);\n"
+	"			font-family: 'JetBrainsMono-Regular';\n"
+	"		}\n\n"
+
+	"		#site {\n"
+	"			width: 80%;\n"
+	"			margin-left: auto;\n"
+	"			margin-right: auto;\n"
+	"			min-width: 1000px;\n"
+	"		}\n\n"
+
+	"		#site-logo {\n"
+	"			text-align: center;\n"
+	"			-webkit-box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
+	"			-moz-box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
+	"			box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
+	"			background-color: rgb(255, 255, 150);\n"
+	"			padding: 10px;\n"
+	"			border-radius: 10px;\n"
+	"			margin: 10px 0 0 0;\n"
+	"			font-family: 'JetBrainsMono-Bold';\n"
+	"		}\n\n"
+
+	"		#site-logo-fileName {\n"
+	"			color: rgb(255, 50, 50);\n"
+	"			height: 60px;\n"
+	"			font-size: 40px;\n"
+	"		}\n\n"
+
+	"		#site-logo-filePath {\n"
+	"			height: 30px;\n"
+	"			font-size: 18px;\n"
+	"		}\n\n"
+
+	"		.site-content-container {\n"
+	"			-webkit-box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
+	"			-moz-box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
+	"			box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
+	"			background-color: rgb(255, 255, 200);\n"
+	"			padding: 10px;\n"
+	"			border-radius: 10px;\n"
+	"			margin: 10px 0 0 0;\n"
+	"		}\n\n"
+
+	"		.site-content-container-containerHeader {\n"
+	"			font-family: 'JetBrainsMono-Bold';\n"
+	"			background-color: rgb(248, 207, 125);\n"
+	"			font-size: 20px;\n"
+	"			text-align: center;\n"
+	"			padding: 7px 0;\n"
+	"		}\n\n"
+
+	"		.site-content-container-containerBody {\n"
+	"			padding: 10px 0;\n"
+	"		}\n\n"
+
+	"		table, th, td {\n"
+	"			border: 1px solid black;\n"
+	"			border-collapse: collapse;\n"
+	"		}\n\n"
+
+	"		#footer {\n"
+	"			margin: 10px 0 0 0;\n"
+	"			text-align: center;\n"
+	"		}\n\n"
+
+	"	</style>\n"
+	"</head>\n\n"
+
+	"<body>\n"
+	"	<div id =\"site\">\n"
+	"		<div id =\"site-logo\">\n"
+	"			<div id =\"site-logo-fileName\">\n"
+	"				<?fileName?>\n"
+	"			</div>\n"
+	"			<div id =\"site-logo-filePath\">\n"
+	"				<?filePath?>\n"
+	"			</div>\n"
+	"		</div>\n\n"
+
+	"		<div id =\"site-content\">\n"
+	"			<?content?>\n"
+	"		</div>\n\n"
+
+	"		<div id =\"footer\">\n"
+	"			Created by : Igor Jarek, <a href =\"https://github.com/igorJarek\" target = \"_blank\">GitHub</a><br/>\n"
+	"			Font : <a href =\"https://www.jetbrains.com/lp/mono/\" target = \"_blank\">JetBrains Mono</a>\n"
+	"		</div>\n"
+	"	</div>\n"
+	"</body>\n"
+	"</html>"
+};
+
+const string CONTAINER_TEMPLATE
+{
+	"<div class =\"site-content-container\">\n"
+	"	<div class =\"site-content-container-containerHeader\">\n"
+	"		<?containerHeader?>\n"
+	"	</div>\n"
+	"	<div class =\"site-content-container-containerBody\">\n"
+	"		<?containerBody?>\n"
+	"	</div>\n"
+	"</div>\n"
+};
+
+const string TABLE_TEMPLATE
+{
+	"<table style = \"width:100%\">\n"
+	"	<?tableBody?>\n"
+	"</table>\n"
+};
+
+const string TABLE_COLUMN_TR_TEMPLATE
+{
+	"<tr>\n"
+	"	<?tableColumnTr?>\n"
+	"</tr>\n"
+};
+
+const string TABLE_COLUMN_TH_TEMPLATE
+{
+	"<th><?tableColumnTh?></th>\n"
+};
+
+const string TABLE_ROW_TR_TEMPLATE
+{
+	"<tr>\n"
+	"	<?tableRowTr?>\n"
+	"</tr>\n"
+};
+
+const string TABLE_ROW_TD_TEMPLATE
+{
+	"<td style =\"text-align:center;\"><?tableRowTd?></td>\n"
+};
--- html/src/Html.cpp
+++ html/src/Html.cpp
@@ -1,167 +1,5 @@
 #include "Html.h"
 
-string const HTMLBuilder::m_indexTemplate
-{  
-	"<!DOCTYPE html>\n"
-	"<html lang=\"en\">\n"
-	"<head>\n"
-	"	<meta charset=\"utf-8\"/>\n"
-	"	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\n"
-
-	"	<title><?title?></title>\n\n"
-
-	"	<style>\n"
-	"		@font-face {\n"
-	"			font-family: 'JetBrainsMono-Regular';\n"
-	"			src: url('https://raw.githubusercontent.com/igorJarek/SourceCodeAnalyzer/master/html/fonts/JetBrainsMono-Regular.woff') format('woff');\n"
-	"			font-weight: normal;\n"
-	"			font-style: normal;\n"
-	"		}\n\n"
-
-	"		@font-face {\n"
-	"			font-family: 'JetBrainsMono-Bold';\n"
-	"			src: url('https://raw.githubusercontent.com/igorJarek/SourceCodeAnalyzer/master/html/fonts/JetBrainsMono-Bold.woff') format('woff');\n"
-	"			font-weight: bold;\n"
-	"			font-style: normal;\n"
-	"		}\n\n"
-
-	"		body {\n"
-	"			background-color: rgb(255, 255, 220);\n"
-	"			font-family: 'JetBrainsMono-Regular';\n"
-	"		}\n\n"
-
-	"		#site {\n"
-	"			width: 80%;\n"
-	"			margin-left: auto;\n"
-	"			margin-right: auto;\n"
-	"			min-width: 1000px;\n"
-	"		}\n\n"
-
-	"		#site-logo {\n"
-	"			text-align: center;\n"
-	"			-webkit-box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
-	"			-moz-box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
-	"			box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
-	"			background-color: rgb(255, 255, 150);\n"
-	"			padding: 10px;\n"
-	"			border-radius: 10px;\n"
-	"			margin: 10px 0 0 0;\n"
-	"			font-family: 'JetBrainsMono-Bold';\n"
-	"		}\n\n"
-
-	"		#site-logo-fileName {\n"
-	"			color: rgb(255, 50, 50);\n"
-	"			height: 60px;\n"
-	"			font-size: 40px;\n"
-	"		}\n\n"
-
-	"		#site-logo-filePath {\n"
-	"			height: 30px;\n"
-	"			font-size: 18px;\n"
-	"		}\n\n"
-
-	"		.site-content-container {\n"
-	"			-webkit-box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
-	"			-moz-box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
-	"			box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.75);\n"
-	"			background-color: rgb(255, 255, 200);\n"
-	"			padding: 10px;\n"
-	"			border-radius: 10px;\n"
-	"			margin: 10px 0 0 0;\n"
-	"		}\n\n"
-
-	"		.site-content-container-containerHeader {\n"
-	"			font-family: 'JetBrainsMono-Bold';\n"
-	"			background-color: rgb(248, 207, 125);\n"
-	"			font-size: 20px;\n"
-	"			text-align: center;\n"
-	"			padding: 7px 0;\n"
-	"		}\n\n"
-
-	"		.site-content-container-containerBody {\n"
-	"			padding: 10px 0;\n"
-	"		}\n\n"
-
-	"		table, th, td {\n"
-	"			border: 1px solid black;\n"
-	"			border-collapse: collapse;\n"
-	"		}\n\n"
-
-	"		#footer {\n"
-	"			margin: 10px 0 0 0;\n"
-	"			text-align: center;\n"
-	"		}\n\n"
-
-	"	</style>\n"
-	"</head>\n\n"
-
-	"<body>\n"
-	"	<div id =\"site\">\n"
-	"		<div id =\"site-logo\">\n"
-	"			<div id =\"site-logo-fileName\">\n"
-	"				<?fileName?>\n"
-	"			</div>\n"
-	"			<div id =\"site-logo-filePath\">\n"
-	"				<?filePath?>\n"
-	"			</div>\n"
-	"		</div>\n\n"
-
-	"		<div id =\"site-content\">\n"
-	"			<?content?>\n"
-	"		</div>\n\n"
-
-	"		<div id =\"footer\">\n"
-	"			Created by : Igor Jarek, <a href =\"https://github.com/igorJarek\" target = \"_blank\">GitHub</a><br/>\n"
-	"			Font : <a href =\"https://www.jetbrains.com/lp/mono/\" target = \"_blank\">JetBrains Mono</a>\n"
-	"		</div>\n"
-	"	</div>\n"
-	"</body>\n"
-	"</html>"
-};
-
-string const HTMLBuilder::m_containerTemplate
-{
-	"<div class =\"site-content-container\">\n"
-	"	<div class =\"site-content-container-containerHeader\">\n"
-	"		<?containerHeader?>\n"
-	"	</div>\n"
-	"	<div class =\"site-content-container-containerBody\">\n"
-	"		<?containerBody?>\n"
-	"	</div>\n"
-	"</div>\n"
-};
-
-string const HTMLBuilder::m_tableTemplate
-{
-	"<table style = \"width:100%\">\n"
-	"	<?tableBody?>\n"
-	"</table>\n"
-};
-
-const string HTMLBuilder::m_tableColumnTrTemplate
-{
-	"<tr>\n"
-	"	<?tableColumnTr?>\n"
-	"</tr>\n"
-};
-
-const string HTMLBuilder::m_tableColumnThTemplate
-{
-	"<th><?tableColumnTh?></th>\n"
-};
-
-const string HTMLBuilder::m_tableRowTrTemplate
-{
-	"<tr>\n"
-	"	<?tableRowTr?>\n"
-	"</tr>\n"
-};
-
-const string HTMLBuilder::m_tableRowTdTemplate
-{
-	"<td style =\"text-align:center;\"><?tableRowTd?></td>\n"
-};
-
 void HTMLBuilder::addTable(const string& contentTitle, vector<string> columnNames)
 {
 	DivDescriptor divDescriptor;
@@ -184,7 +22,7 @@ void HTMLBuilder::addTableRow(vector<string> rowsContent)
 
 void HTMLBuilder::buildPage(string& pageContent)
 {
-	pageContent = m_indexTemplate;
+	pageContent = INDEX_TEMPLATE;
 
 	string titleKeyword		{ "<?title?>" };
 	string fileNameKeyword	{ "<?fileName?>" };
@@ -205,7 +43,7 @@ void HTMLBuilder::buildContent(string& divContent, const size_t tabsCount)
 {
 	for (DivDescriptor& divDescriptor : m_divDescriptorVec)
 	{
-		string divContainer				{ m_containerTemplate };
+		string divContainer				{ CONTAINER_TEMPLATE };
 		string containerHeaderKeyword	{ "<?containerHeader?>" };
 		string containerBodyKeyword		{ "<?containerBody?>" };
 
@@ -235,7 +73,7 @@ void HTMLBuilder::buildTable(string& divContainer, const DivDescriptor& divDescr
 {
 	const TableContent& tableContentVecElem = m_tableContentVec.at(divDescriptor.vectorIndex);
 
-	string tableContent			{ m_tableTemplate };
+	string tableContent			{ TABLE_TEMPLATE };
 	string tableBodyKeyword		{ "<?tableBody?>" };
 
 	string tableColumnTrKeyword	{ "<?tableColumnTr?>" };
@@ -246,13 +84,13 @@ void HTMLBuilder::buildTable(string& divContainer, const DivDescriptor& divDescr
 	size_t tableBodyTabsCount	{ tabsCount + countTabs(tableContent, tableBodyKeyword) };
 
 	// Columns
-	string tableColumnsTr{ m_tableColumnTrTemplate };
+	string tableColumnsTr{ TABLE_COLUMN_TR_TEMPLATE };
 	size_t tableColumnTrTabsCount{ countTabs(tableColumnsTr, tableColumnTrKeyword) };
 
 	string tableColumnsThs;
 	for (const string& columnName : tableContentVecElem.columnNames)
 	{
-		string tableColumnsTh{ m_tableColumnThTemplate };
+		string tableColumnsTh{ TABLE_COLUMN_TH_TEMPLATE };
 		replaceKeyword(tableColumnsTh, tableColumnThKeyword, columnName, true, tableBodyTabsCount + tableColumnTrTabsCount);
 		tableColumnsThs += tableColumnsTh;
 	}
@@ -263,13 +101,13 @@ void HTMLBuilder::buildTable(string& divContainer, const DivDescriptor& divDescr
 	string tableRowsTrs;
 	for (const vector<string>& row : tableContentVecElem.rows)
 	{
-		string tableRowsTr{ m_tableRowTrTemplate };
+		string tableRowsTr{ TABLE_ROW_TR_TEMPLATE };
 		size_t tableRowsTrTabsCount{ countTabs(tableRowsTr, tableRowTrKeyword) };
 
 		string tableRowsTds;
 		for (const string& rowName : row)
 		{
-			string tableRowsTd{ m_tableRowTdTemplate };
+			string tableRowsTd{ TABLE_ROW_TD_TEMPLATE };
 			replaceKeyword(tableRowsTd, tableRowTdKeyword, rowName, true, tableBodyTabsCount + tableRowsTrTabsCount);
 
 			tableRowsTds += tableRowsTd;
