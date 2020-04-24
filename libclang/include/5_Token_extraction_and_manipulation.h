c6cc2e32988ad675f92288b96d550525ff4d3e21
igorJarek <igij@o2.pl>
2020-04-25 00:18:47 +0200

    libclang: add 5_Token_extraction_and_manipulation (2)

--- libclang/include/5_Token_extraction_and_manipulation.h
+++ libclang/include/5_Token_extraction_and_manipulation.h
@@ -2,6 +2,9 @@
 
 #include <clang-c/Index.h>
 #include "UtilityFunctions.h"
+#include "StructsEnums2String.h"
+#include "8_File_manipulation_routines.h"
+#include "21_Physical_source_locations.h"
 #include <fstream>
 #include <string>
 #include <iostream>
@@ -11,18 +14,18 @@ using namespace std;
 /*
 
     Used:
-
-
-    Unused:
-
-        1. CXToken * 	        clang_getToken          (CXTranslationUnit TU, CXSourceLocation Location)
         2. CXTokenKind 	        clang_getTokenKind      (CXToken)
         3. CXString 	        clang_getTokenSpelling  (CXTranslationUnit, CXToken)
-        4. CXSourceLocation 	clang_getTokenLocation  (CXTranslationUnit, CXToken)
         5. CXSourceRange 	    clang_getTokenExtent    (CXTranslationUnit, CXToken)
         6. void 	            clang_tokenize          (CXTranslationUnit TU, CXSourceRange Range, CXToken **Tokens, unsigned *NumTokens)
         7. void 	            clang_annotateTokens    (CXTranslationUnit TU, CXToken *Tokens, unsigned NumTokens, CXCursor *Cursors)
         8. void 	            clang_disposeTokens     (CXTranslationUnit TU, CXToken *Tokens, unsigned NumTokens)
+
+    Unused:
+
+        1. CXToken * 	        clang_getToken          (CXTranslationUnit TU, CXSourceLocation Location)
+        4. CXSourceLocation 	clang_getTokenLocation  (CXTranslationUnit, CXToken)
+
 */
 
 void _5_token_extraction(const CXTranslationUnit& translationUnit, const string& filePath);
--- libclang/src/5_Token_extraction_and_manipulation.cpp
+++ libclang/src/5_Token_extraction_and_manipulation.cpp
@@ -5,9 +5,76 @@ void _5_token_extraction(const CXTranslationUnit& translationUnit, const string&
 	string strData{ "Token extraction and manipulation : \n" };
 	uint32_t tOffset{ 1 };
 
+	CXFile file = _8_getFile(translationUnit, filePath);
 
+	int64_t lineCount = countFileLines(filePath);
+	if (lineCount == -1)
+		strData += tabOffset(tOffset) + "File is empty." + '\n';
+	else
+	{
+		int64_t lastLineColumns = countFileLineColumns(filePath, lineCount);
+		if(lastLineColumns == -1)
+			strData += tabOffset(tOffset) + "Something is wrong with last line oraz file." + '\n';
+		else
+		{
+			//  For first char '#' CXSourceLocation is <1, 1>
+			//  For last  char ';' CXSourceLocation is <11, 2>
+			/*
+				1. #pragma once
+				2.
+				3. class Class
+				4. {
+				5.		public:
+				6.			Class(void);
+				7.			~Class(void);
+				8.			Class(const Class& classes);
+				9.
+				10.			void classMethod(void);
+				11.	};
+			*/
+
+			CXSourceLocation beginLocation = _21_getLocation(translationUnit, file, 1, 1);
+			CXSourceLocation endLocation   = _21_getLocation(translationUnit, file, lineCount, lastLineColumns);
+
+			CXSourceRange tokenizerRange = _21_getRange(beginLocation, endLocation);
+
+			CXCursor* cursorsOut = nullptr;
+			CXToken*  tokensOut = nullptr;
+			uint32_t  tokensNum = 0;
+
+			clang_tokenize(translationUnit, tokenizerRange, &tokensOut, &tokensNum);											// 6.
+
+			if (tokensNum > 0)
+			{
+				strData += tabOffset(tOffset) + "Token count : " + to_string(tokensNum) + '\n';
+				strData += tabOffset(tOffset) + "Tokens :" + '\n';
+
+				clang_annotateTokens(translationUnit, tokensOut, tokensNum, cursorsOut);										// 7.
+
+				for (uint32_t index{0}; index < tokensNum; ++index)
+				{
+					const CXToken token = tokensOut[index];
+					
+					CXTokenKind			tokenKind		= clang_getTokenKind(token);											// 2.
+					CXString			tokenSpelling	= clang_getTokenSpelling(translationUnit, token);						// 3.
+					CXSourceLocation	tokenLoc		= clang_getTokenLocation(translationUnit, token);						// 4.   unused
+					CXSourceRange		tokenRange		= clang_getTokenExtent(translationUnit, token);							// 5.
+
+					strData += 
+						tabOffset(tOffset + 1) +  to_string(index + 1) + ")\t" +  _11_CXString2String(tokenSpelling) + '\n' +
+						tabOffset(tOffset + 2) + "clang_getTokenKind: " + CXTokenKind2String(tokenKind) + '\n' +
+						tabOffset(tOffset + 2) + "clang_getTokenExtent: " + '\n' +
+						CXSourceRange2String(tokenRange, tOffset + 3);
+				}
+			}
+
+			if (tokensOut)
+				clang_disposeTokens(translationUnit, tokensOut, tokensNum);														// 8.
+		}
+		
+	}
 
 	string saveFilePath{ filePath + ".tokens" };
-	if (!saveToFile(saveFilePath, strData))
-		cout << "Couldn't create file : " << saveFilePath << endl;
+		if (!saveToFile(saveFilePath, strData))
+			cout << "Couldn't create file : " << saveFilePath << endl;
 }
