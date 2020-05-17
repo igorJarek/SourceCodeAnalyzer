45abbf0bdf7a41984d51fda2438fe01bf16f304f
igorJarek <igij@o2.pl>
2020-05-17 22:23:58 +0200

    libclang: add _11_ Clang functions

--- libclang/include/11_String_manipulation_routines.h
+++ libclang/include/11_String_manipulation_routines.h
@@ -11,7 +11,7 @@ using namespace std;
 /*
     Used:
 
-	        1. const char*  clang_getCString        (CXString string)
+            1. const char*  clang_getCString        (CXString string)
             2. void         clang_disposeString     (CXString string)
             3. void         clang_disposeStringSet  (CXStringSet *set)
 
@@ -23,3 +23,7 @@ string _11_CXString2String(const CXString& str);
 void _11_releaseCXStringSet(CXStringSet* stringSet);
 vector<string> _11_CXStringSet2StringVec(CXStringSet* stringSet);
 ostream& operator<<(ostream& stream, const CXString& str);
+
+const char *    _11_getCString          (CXString string);
+void            _11_disposeString       (CXString string);
+void            _11_disposeStringSet    (CXStringSet *set);
--- libclang/src/11_String_manipulation_routines.cpp
+++ libclang/src/11_String_manipulation_routines.cpp
@@ -42,3 +42,12 @@ void _11_releaseCXStringSet(CXStringSet* stringSet)
         clang_disposeStringSet(stringSet); // 3
     */
 }
+
+const char *    _11_getCString          (CXString string)
+                                        { return clang_getCString(string); }
+
+void            _11_disposeString       (CXString string)
+                                        { return clang_disposeString(string); }
+
+void            _11_disposeStringSet    (CXStringSet *set)
+                                        { return clang_disposeStringSet(set); }
