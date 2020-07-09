9f87ab5c85ab664f18a0ee3422eb28e4d40dabac
igorJarek <igij@o2.pl>
2020-07-10 01:12:08 +0200

    libclang: refactorized, cleaned up 17_Debugging_facilities.h

--- libclang/include/17_Debugging_facilities.h
+++ libclang/include/17_Debugging_facilities.h
@@ -7,8 +7,11 @@
 
 using namespace std;
 
-CXString    _17_getCursorKindSpelling               (enum CXCursorKind Kind);                                                                               // 1.
+// Clang Functions
+// Only for libclang library debugging
+
+CXString    _17_getCursorKindSpelling               (enum CXCursorKind Kind);                                                                                   // 1.
 void        _17_getDefinitionSpellingAndExtent      (CXCursor C, const char **startBuf, const char **endBuf, unsigned *startLine, unsigned *startColumn, 
-                                                     unsigned *endLine, unsigned *endColumn);                                                               // 2.
-void        _17_enableStackTraces                   (void);                                                                                                 // 3.
-void        _17_executeOnThread                     (void(*fn)(void *), void *user_data, unsigned stack_size);                                              // 4.
+                                                     unsigned *endLine, unsigned *endColumn);                                                                   // 2.
+void        _17_enableStackTraces                   (void);                                                                                                     // 3.
+void        _17_executeOnThread                     (void(*fn)(void *), void *user_data, unsigned stack_size);                                                  // 4.
