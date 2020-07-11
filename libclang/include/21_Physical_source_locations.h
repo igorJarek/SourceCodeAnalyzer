a16037bd1ed850dfcfa35c06476dc30b2795b8b4
igorJarek <igij@o2.pl>
2020-07-11 22:10:24 +0200

    libclang: refactorized, cleaned up 21_Physical_source_locations.h

--- libclang/include/21_Physical_source_locations.h
+++ libclang/include/21_Physical_source_locations.h
@@ -9,31 +9,33 @@ using namespace std;
 
 /*
 
-	Used:
+    Used:
 
-	Unused:
+    Unused:
 
-		1. CXSourceLocation 	clang_getNullLocation               (void)
-		2. unsigned 	        clang_equalLocations                (CXSourceLocation loc1, CXSourceLocation loc2)
-		3. CXSourceLocation 	clang_getLocation                   (CXTranslationUnit tu, CXFile file, unsigned line, unsigned column)
-		4. CXSourceLocation 	clang_getLocationForOffset          (CXTranslationUnit tu, CXFile file, unsigned offset)
-		5. int 	                clang_Location_isInSystemHeader     (CXSourceLocation location)
-		6. int 	                clang_Location_isFromMainFile       (CXSourceLocation location)
-		7. CXSourceRange 	    clang_getNullRange                  (void)
-		8. CXSourceRange 	    clang_getRange                      (CXSourceLocation begin, CXSourceLocation end)
-		9. unsigned 	        clang_equalRanges                   (CXSourceRange range1, CXSourceRange range2)
-		10. int 	            clang_Range_isNull                  (CXSourceRange range)
-		11. void 	            clang_getExpansionLocation          (CXSourceLocation location, CXFile *file, unsigned *line, unsigned *column, unsigned *offset)
-		12. void 	            clang_getPresumedLocation           (CXSourceLocation location, CXString *filename, unsigned *line, unsigned *column)
-		13. void 	            clang_getInstantiationLocation      (CXSourceLocation location, CXFile *file, unsigned *line, unsigned *column, unsigned *offset)
-		14. void 	            clang_getSpellingLocation           (CXSourceLocation location, CXFile *file, unsigned *line, unsigned *column, unsigned *offset)
-		15. void 	            clang_getFileLocation               (CXSourceLocation location, CXFile *file, unsigned *line, unsigned *column, unsigned *offset)
-		16. CXSourceLocation 	clang_getRangeStart                 (CXSourceRange range)
-		17. CXSourceLocation 	clang_getRangeEnd                   (CXSourceRange range)
-		18. CXSourceRangeList* 	clang_getSkippedRanges              (CXTranslationUnit tu, CXFile file)
-		19. CXSourceRangeList* 	clang_getAllSkippedRanges           (CXTranslationUnit tu)
-		20. void                clang_disposeSourceRangeList        (CXSourceRangeList *ranges)
+        1. CXSourceLocation     clang_getNullLocation               (void)
+        2. unsigned             clang_equalLocations                (CXSourceLocation loc1, CXSourceLocation loc2)
+        3. CXSourceLocation     clang_getLocation                   (CXTranslationUnit tu, CXFile file, unsigned line, unsigned column)
+        4. CXSourceLocation     clang_getLocationForOffset          (CXTranslationUnit tu, CXFile file, unsigned offset)
+        5. int                  clang_Location_isInSystemHeader     (CXSourceLocation location)
+        6. int                  clang_Location_isFromMainFile       (CXSourceLocation location)
+        7. CXSourceRange        clang_getNullRange                  (void)
+        8. CXSourceRange        clang_getRange                      (CXSourceLocation begin, CXSourceLocation end)
+        9. unsigned             clang_equalRanges                   (CXSourceRange range1, CXSourceRange range2)
+        10. int                 clang_Range_isNull                  (CXSourceRange range)
+        11. void                clang_getExpansionLocation          (CXSourceLocation location, CXFile *file, unsigned *line, unsigned *column, unsigned *offset)
+        12. void                clang_getPresumedLocation           (CXSourceLocation location, CXString *filename, unsigned *line, unsigned *column)
+        13. void                clang_getInstantiationLocation      (CXSourceLocation location, CXFile *file, unsigned *line, unsigned *column, unsigned *offset)
+        14. void                clang_getSpellingLocation           (CXSourceLocation location, CXFile *file, unsigned *line, unsigned *column, unsigned *offset)
+        15. void                clang_getFileLocation               (CXSourceLocation location, CXFile *file, unsigned *line, unsigned *column, unsigned *offset)
+        16. CXSourceLocation    clang_getRangeStart                 (CXSourceRange range)
+        17. CXSourceLocation    clang_getRangeEnd                   (CXSourceRange range)
+        18. CXSourceRangeList*  clang_getSkippedRanges              (CXTranslationUnit tu, CXFile file)
+        19. CXSourceRangeList*  clang_getAllSkippedRanges           (CXTranslationUnit tu)
+        20. void                clang_disposeSourceRangeList        (CXSourceRangeList *ranges)
 */
+
+// Clang Functions
 
 CXSourceLocation        _21_getNullLocation             (void);                                                                                         // 1.
 unsigned                _21_equalLocations              (CXSourceLocation loc1, CXSourceLocation loc2);                                                 // 2.
