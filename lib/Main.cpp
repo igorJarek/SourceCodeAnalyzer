ec82d95f7e28b3d1ab17d719550151161dd4ac97
igorJarek <igij@o2.pl>
2020-08-28 23:53:31 +0200

    lib: refactorized names, small fixes

--- lib/Main.cpp
+++ lib/Main.cpp
@@ -5,12 +5,12 @@ int main(void)
 {
     testFunction(1, 2, 3, 4);
 
-    TestClass class_1;
-    class_1.testClassMethod();
+    TestClass testClass;
+    testClass.testClassMethod();
 
-    TestClass* class_2 = new TestClass();
-    class_2->testClassMethod();
-    delete class_2;
+    TestClass* testClassPtr = new TestClass();
+    testClassPtr->testClassMethod();
+    delete testClassPtr;
 
     return 0;
 }
