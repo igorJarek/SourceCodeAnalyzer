0a0671d5794dec710616e4b2f65d29efd827ce3e
igorJarek <igij@o2.pl>
2020-12-05 17:28:44 +0100

    utility: add SourceCode class

---
+++ utility/include/SourceCode/SourceCode.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <clang-c/Index.h>
+
+#include <list>
+#include <string>
+#include <memory>
+#include <functional>
+
+using std::list;
+using std::string;
+using std::shared_ptr;
+using std::function;
+
+struct ASTNode
+{
+    shared_ptr<ASTNode>       findChild(enum CXCursorKind cursorKind);
+
+    shared_ptr<ASTNode>       parent;
+    list<shared_ptr<ASTNode>> childrens;
+
+    CXCursor                  cursor;
+};
+
+struct AST
+{
+    public:
+        AST();
+        ~AST();
+
+    public:
+        uint32_t getNodesCount() const { return m_nodes; }
+
+        void     addCXCursor(uint32_t level, CXCursor cursor);
+        void     traversingAST(function<void (shared_ptr<ASTNode>)> traversingFunc);
+
+        uint32_t getNestingLevel()      const { return m_nestingLevel; }
+        void     increaseNestingLevel()       { m_nestingLevel++; }
+        void     decreaseNestingLevel()       { m_nestingLevel--; }
+
+    private:    
+        void     traversingNode(function<void (shared_ptr<ASTNode>)> traversingFunc, shared_ptr<ASTNode>& node);
+
+    private:
+        shared_ptr<ASTNode> m_root;
+        uint32_t            m_nodes = 0;
+
+        uint32_t            m_nestingLevel = 0;
+};
+
+class ClientData
+{
+public:
+    ClientData(AST* ast) : 
+        m_ast(ast) 
+    { }
+
+    AST* m_ast;
+};
+
+struct Tokens
+{
+    int64_t  fileLines           = 0;
+    int64_t  fileLastLineColumns = 0;
+    CXToken* tokens              = nullptr;
+    uint32_t tokensCount         = 0;
+};
+
+class SourceCode
+{
+    public:
+        SourceCode(const string& filePath, const char* compilation_args[], uint16_t argsCount);
+        ~SourceCode();
+
+        static int64_t countFileLines(const string& filePath);
+        static int64_t countFileLineColumns(const string& filePath, int64_t line);
+
+    public:
+        AST&    getAST()    { return m_ast; }
+        Tokens& getTokens() { return m_tokens; }
+
+    private:
+        const string&       m_filePath;
+
+        CXIndex             m_index;
+        CXErrorCode         m_compilationErrorCode;
+        CXTranslationUnit   m_translationUnit;
+        uint32_t            m_compilationErrorCount;
+
+        AST                 m_ast;
+        Tokens              m_tokens;
+};
---
+++ utility/src/SourceCode/SourceCode.cpp
@@ -0,0 +1,182 @@
+#include <SourceCode/SourceCode.hpp>
+#include <fstream>
+
+CXChildVisitResult visitor(CXCursor cursor, CXCursor /* parent */, CXClientData client_data);
+
+shared_ptr<ASTNode> ASTNode::findChild(enum CXCursorKind cursorKind)
+{
+    for(shared_ptr<ASTNode> elem : childrens)
+    {
+        if(elem->cursor.kind == cursorKind)
+            return elem;
+        else
+            return elem->findChild(cursorKind);
+    }
+
+    return nullptr;
+}
+
+AST::AST()
+{
+    m_root = std::make_shared<ASTNode>();
+    m_root->parent = nullptr;
+}
+
+AST::~AST()
+{
+    
+}
+
+void AST::addCXCursor(uint32_t level, CXCursor cursor)
+{
+    shared_ptr<ASTNode> currentNode = m_root;
+
+    for(uint32_t indexLevel = 0; indexLevel < level; ++indexLevel)
+    {
+        if(!currentNode->childrens.empty())
+            currentNode = currentNode->childrens.back();
+    }
+
+    shared_ptr<ASTNode> newNode = std::make_shared<ASTNode>();
+    newNode->parent = currentNode;
+    newNode->cursor = cursor;
+
+    currentNode->childrens.push_back(newNode);
+
+    ++m_nodes;
+}
+
+void AST::traversingAST(function<void (shared_ptr<ASTNode>)> traversingFunc)
+{
+    auto& rootChildren = m_root->childrens;
+    for(auto& elem : rootChildren)
+        traversingNode(traversingFunc, elem);
+}
+
+void AST::traversingNode(function<void (shared_ptr<ASTNode>)> traversingFunc, shared_ptr<ASTNode>& node)
+{
+    if(traversingFunc)
+        traversingFunc(node);
+
+    auto& nodeChildren = node->childrens;
+    for(auto& elem : nodeChildren)
+        traversingNode(traversingFunc, elem);
+}
+
+SourceCode::SourceCode(const string& filePath, const char* compilation_args[], uint16_t argsCount) :
+    m_filePath(filePath)
+{
+    m_index = clang_createIndex(0, 0);
+
+    m_compilationErrorCode = clang_parseTranslationUnit2(m_index, 
+                                                         m_filePath.c_str(), 
+                                                         compilation_args, 
+                                                         argsCount,
+                                                         nullptr,
+                                                         0,
+                                                         CXTranslationUnit_None,
+                                                         &m_translationUnit);
+
+    m_compilationErrorCount = clang_getNumDiagnostics(m_translationUnit);
+
+    // AST
+
+    if (m_compilationErrorCode == CXError_Success &&
+        m_compilationErrorCount == 0)
+    {
+        ClientData clientData(&m_ast);
+
+        CXCursor cursor = clang_getTranslationUnitCursor(m_translationUnit);
+        clang_visitChildren(cursor, visitor, &clientData);
+    }
+
+    // tokenize whole file
+
+    m_tokens.fileLines = countFileLines(filePath);
+    if (m_tokens.fileLines != -1)
+    {
+        m_tokens.fileLastLineColumns = countFileLineColumns(filePath, m_tokens.fileLines);
+        if(m_tokens.fileLastLineColumns != -1)
+        {
+            CXFile           file           = clang_getFile(m_translationUnit, filePath.c_str());
+            CXSourceLocation beginLocation  = clang_getLocation(m_translationUnit, file, 1, 1);
+            CXSourceLocation endLocation    = clang_getLocation(m_translationUnit, file, static_cast<unsigned int>(m_tokens.fileLines), static_cast<unsigned int>(m_tokens.fileLastLineColumns));
+            CXSourceRange    tokenizerRange = clang_getRange(beginLocation, endLocation);
+
+            clang_tokenize(m_translationUnit, tokenizerRange, &m_tokens.tokens, &m_tokens.tokensCount);
+        }
+    }
+}
+
+SourceCode::~SourceCode()
+{
+    clang_disposeTranslationUnit(m_translationUnit);
+    clang_disposeIndex(m_index);
+}
+
+CXChildVisitResult visitor(CXCursor cursor, CXCursor /* parent */, CXClientData client_data)
+{
+    ClientData* clientDataPtr = reinterpret_cast<ClientData*>(client_data);
+    AST* ast    = clientDataPtr->m_ast;
+
+    ast->addCXCursor(ast->getNestingLevel(), cursor);
+
+    ast->increaseNestingLevel();
+
+    clang_visitChildren(cursor, visitor, client_data);
+
+    ast->decreaseNestingLevel();
+
+    return CXChildVisit_Continue;
+}
+
+int64_t SourceCode::countFileLines(const string& filePath)
+{
+    std::fstream stream;
+    int64_t lines{ -1 };
+
+    stream.open(filePath, std::fstream::in);
+    if (stream.is_open())
+    {
+        lines = 0;
+
+        string s;
+        while (getline(stream, s))
+            ++lines;
+
+        stream.close();
+    }
+
+    return lines;
+}
+
+int64_t SourceCode::countFileLineColumns(const string& filePath, int64_t line)
+{
+    if (line < 0)
+        return -1;
+
+    std::fstream stream;
+    int64_t columns{ -1 };
+    int64_t lines{ -1 };
+
+    stream.open(filePath, std::fstream::in);
+    if (stream.is_open())
+    {
+        string str;
+        lines = 0;
+
+        while (getline(stream, str))
+        {
+            ++lines;
+            if (lines == line)
+                break;
+        }
+
+        if(lines == line) // file lines < line
+            columns = str.size();
+
+        stream.close();
+    }
+
+    return columns;
+}
