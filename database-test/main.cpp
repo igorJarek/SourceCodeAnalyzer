19cd260d794adaeb4e5b5ff1307ea22b12a49f8e
igorJarek <igij@o2.pl>
2020-12-07 22:36:36 +0100

    database-test: move program logic to DatabaseBuilder

--- database-test/main.cpp
+++ database-test/main.cpp
@@ -1,158 +1,27 @@
-#include <SourceCode/SourceCode.hpp>
-
-#include <iostream>
-#include <map>
-#include <FolderBrowser/FolderBrowser.h>
-#include <ExecutionTimeMeasurement/ExecutionTimeMeasurement.h>
-#include <LibClangStruct2Str/LibClangStruct2Str.h>
-
-using std::map;
-using std::to_string;
-
-struct FunctionPos
-{
-    void setCXSourceRange(const CXSourceRange& range)
-    {
-        CXSourceLocation locStart  = clang_getRangeStart(range);
-        CXSourceLocation locEnd    = clang_getRangeEnd(range);
-
-        clang_getExpansionLocation(locStart, nullptr, &startLine, &startCol, nullptr);
-        clang_getExpansionLocation(locEnd,   nullptr, &endLine, &endCol, nullptr);
-    }
-
-    uint32_t startLine, startCol;
-    uint32_t endLine, endCol;
-};
-
-struct Function             // clang_getCursorReferenced, clang_isCursorDefinition, clang_equalCursors, clang_getCanonicalCursor (dla definicji zwroci plik .hpp)
-                            // clang_getCursorResultType, clang_getNumArgTypes
-{
-    uint8_t     kind;       // 0 - empty, 1 - function, 2 - method, 3 - invoke
-
-    string      usr;        // clang_getCursorUSR
-    string      mangling;   // clang_Cursor_getMangling
-    FunctionPos pos;        // clang_getCursorLocation ?
-
-    string      name;       // clang_getCursorSpelling ?
-    uint8_t     argCount;   // clang_Cursor_getNumArguments
-};
+#include <DatabaseBuilder/DatabaseBuilder.hpp>
 
 int main()
 {
     const string APP_NAME("database-test");
     const string APP_VERSION("1.0");
 
-    const string libPath("..\\lib\\");
-    const string databasePath("..\\lib\\lib.sqlite3");
+    const string LIB_PATH("..\\lib\\");
+    const string DATABASE_PATH("..\\lib\\lib.sqlite3");
 
-    const char* argsIncludePrefix       = "-I";
-    const char* argsIncludePath         = "..\\lib\\include\\";
-    const char* COMPILATION_ARGS[2]     = { argsIncludePrefix, argsIncludePath };
+    const char* ARGS_INCLUDE_PREFIX = "-I";
+    const char* ARGS_INCLUDE_PATH   = "..\\lib\\include\\";
+    const char* COMPILATION_ARGS[2] = { ARGS_INCLUDE_PREFIX, ARGS_INCLUDE_PATH };
 
-    FolderBrowser fb;
-    fb.clearFileList();
-    fb.setFileTypeBrowser(FileType::SOURCE_FILE);
+    FolderBrowser folderBrowser;
+    folderBrowser.clearFileList();
+    folderBrowser.setFileTypeBrowser(FileType::SOURCE_FILE);
+    folderBrowser.addIgnoreFilePath("..\\lib\\src\\TestPrimitives.cpp");
+    folderBrowser.startFolderBrowse(LIB_PATH);
 
-    fb.addIgnoreFilePath("..\\lib\\src\\TestPrimitives.cpp");
+    Database database(DATABASE_PATH);
 
-    fb.startFolderBrowse(libPath);
-
-    const size_t        fileCount = fb.getFileCount();
-    const list<string>& fileList  = fb.getFileList();
-
-    for(const string& filePath : fileList)
-    {
-        ExecutionTimeMeasurement timeMeasurement("Parsing " + filePath + " file in");
-
-        map<string, Function> functionsDefMap;
-
-        SourceCode sourceCode(filePath, COMPILATION_ARGS, 2);
-        AST&       ast    = sourceCode.getAST();
-        Tokens&    tokens = sourceCode.getTokens();
-
-        ast.traversingAST
-        (
-            [&functionsDefMap](shared_ptr<ASTNode> astNode) -> void
-            {
-                CXCursor     cursor     = astNode->cursor;
-                CXCursorKind cursorKind = clang_getCursorKind(cursor);
-
-                if(cursorKind == CXCursor_FunctionDecl)
-                {
-                    if(clang_isCursorDefinition(cursor))
-                    {
-                        Function function;
-
-                        function.kind     = 1;
-
-                        function.usr      = to_string(clang_getCursorUSR(cursor));
-                        function.mangling = to_string(clang_Cursor_getMangling(cursor));
-                        function.pos.setCXSourceRange(clang_getCursorExtent(cursor));
-
-                        function.name     = to_string(clang_getCursorSpelling(cursor));
-                        function.argCount = clang_Cursor_getNumArguments(cursor);
-
-                        functionsDefMap.insert( {function.usr, function} );
-                    }
-                }
-                else if(cursorKind == CXCursor_CXXMethod)
-                {
-                    if(clang_isCursorDefinition(cursor))
-                    {
-                        Function function;
-
-                        function.kind     = 2;
-
-                        function.usr      = to_string(clang_getCursorUSR(cursor));
-                        function.mangling = to_string(clang_Cursor_getMangling(cursor));
-                        function.pos.setCXSourceRange(clang_getCursorExtent(cursor));
-
-                        function.name     = to_string(clang_getCursorSpelling(cursor));
-                        function.argCount = clang_Cursor_getNumArguments(cursor);
-
-                        functionsDefMap.insert( {function.usr, function} );
-                    }
-                }
-                else if(cursorKind == CXCursor_CallExpr)
-                {
-                    Function function;
-
-                    function.kind     = 3;
-
-                    function.pos.setCXSourceRange(clang_getCursorExtent(cursor));
-
-                    function.name     = to_string(clang_getCursorSpelling(cursor));
-                    function.argCount = clang_Cursor_getNumArguments(cursor);
-
-                    CXCursor     ref        = clang_getCursorReferenced(cursor);
-                    CXCursorKind cursorRefKind = clang_getCursorKind(ref);
-
-                    if(!clang_isInvalid(cursorRefKind) && cursorRefKind != CXCursor_Constructor && cursorRefKind != CXCursor_Destructor)
-                    {
-                        if(!clang_equalCursors(cursor, ref))
-                        {
-                            function.usr      = to_string(clang_getCursorUSR(ref));
-                            function.mangling = to_string(clang_Cursor_getMangling(ref));
-
-                            functionsDefMap.insert( {function.usr, function} );
-                        }
-                    }
-                }
-            }
-        );
-
-        if(functionsDefMap.size())
-        {
-            cout << "\tResult : " << endl; 
-
-            for(auto& elem : functionsDefMap)
-            {
-                cout << "\t\t";
-                cout << (elem.second.kind == 3 ? "Invoke : " : "Def : ");
-                cout << elem.first << " -> " << elem.second.name << endl;
-            }
-        }
-    }
+    DatabaseBuilder databaseBuilder(database, APP_NAME, APP_VERSION, folderBrowser, COMPILATION_ARGS, 2);
+    databaseBuilder.buildDatabase();
 
     return EXIT_SUCCESS;
 }
