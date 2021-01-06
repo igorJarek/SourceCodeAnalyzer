e3e8646d26ffa3af57da0110b72ba98e241ab53d
igorJarek <igij@o2.pl>
2021-01-06 14:55:45 +0100

    analyzer: fixed wrong SourceCodeView display

--- analyzer/src/SourceCodeView.cpp
+++ analyzer/src/SourceCodeView.cpp
@@ -297,8 +297,6 @@ void SourceCodeView::generateConnectionLines()
 
 void SourceCodeView::draw(QPainter& painter, double zoom)
 {
-    painter.fillRect(QRect(0, 0, 3000, 5000), QColor(30, 30, 30));
-
     painter.scale(zoom, zoom);
 
     for(const SourceCodeBlockVecPtr& sourceCodeBlockVec : m_functionSourceCodeBlockVec)
@@ -336,6 +334,8 @@ void SourceCodeView::saveToSVG(const QString& filePath)
     generator.setDescription("An SVG drawing created by the SVG Generator Example provided with Qt.");
     
     QPainter painter(&generator);
+
+    painter.fillRect(QRect(0, 0, 3000, 5000), QColor(30, 30, 30));
 
     draw(painter, 1.0);
 
